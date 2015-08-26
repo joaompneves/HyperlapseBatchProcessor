3cfb36c7705182670afebe876d01bb045b0a034f
João Neves <joaompnevez@gmail.com>
2015-08-27 00:17:02 +0100

    Initial commit

---
+++ HyperlapseWrapper.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Research.Hyperlapse.Desktop;
+using Microsoft.Research.VisionTools.Toolkit.Desktop.Native;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace HyperlapseBatchProcessor
+{
+    class HyperlapseWrapper
+    {
+        private static AutoResetEvent processingEvent = new AutoResetEvent(false);
+
+        private static VideoReader videoReader = new VideoReader();
+        private static CalibrationProvider calibrationProvider = new CalibrationProvider();
+        private static CalibrationMatcher calibrationMatcher = new CalibrationMatcher(calibrationProvider);
+        private static VideoBitrateEstimator videoBitrateEstimator = new VideoBitrateEstimator();
+
+        public static void ProcessFiles(FileInfo[] files, int speedupFactor, Rational outputFramesPerSecond)
+        {
+            var i = 1;
+            var filesCount = files.Count();
+            var engine = new HyperlapseEngine();
+
+            engine.ProcessingCancelled += OnEngineProcessingCancelled;
+            engine.ProcessingFailed += OnEngineProcessingFailed;
+            engine.ProcessingFinished += OnEngineProcessingFinished;
+            engine.ProgressChanged += OnEngineProgressChanged;
+            engine.TrialStatusChanged += OnEngineTrialStatusChanged;
+
+            foreach (var file in files)
+            {
+                Console.WriteLine("[Processing file " + i++ + " of " + filesCount + "] - " + file.Name + " ");
+
+                var fileOutput = new FileInfo(Path.Combine(file.DirectoryName, "Output", file.Name));
+                if (!fileOutput.Directory.Exists)
+                {
+                    fileOutput.Directory.Create(); // create output dir
+                }
+
+                Process(engine, file, fileOutput, speedupFactor, outputFramesPerSecond);
+            }
+
+            engine.Dispose();
+        }
+
+        private static void Process(HyperlapseEngine engine, FileInfo fileInput, FileInfo fileOutput, int speedupFactor, Rational outputFramesPerSecond)
+        {
+            var hyperlapseParameters = new HyperlapseParameters();
+
+            var videoInfo = videoReader.ReadInfoFromFile(fileInput.FullName);
+            var calibrationInfoForVideo = calibrationMatcher.FindCalibrationInfoForVideo(videoInfo);
+
+            hyperlapseParameters.CalibrationFile = calibrationInfoForVideo.Calibration;
+
+            hyperlapseParameters.StartFrame = 0;
+            hyperlapseParameters.EndFrame = int.MaxValue; // all video
+            hyperlapseParameters.VideoMode = calibrationInfoForVideo.VideoMode;
+            hyperlapseParameters.FrameRate = videoInfo.FramesPerSecond;
+
+            hyperlapseParameters.OutputHeight = videoInfo.Height;
+            hyperlapseParameters.OutputRotation = videoInfo.Rotation;
+            hyperlapseParameters.OutputWidth = videoInfo.Width;
+            hyperlapseParameters.OutputBitrate = (int)videoBitrateEstimator.EstimateBitsPerSecond(videoInfo.BitsPerSecond,
+                                                                                                   videoInfo.Width,
+                                                                                                   videoInfo.Height,
+                                                                                                   videoInfo.Width,
+                                                                                                   videoInfo.Height,
+                                                                                                   videoInfo.FramesPerSecond,
+                                                                                                   outputFramesPerSecond ?? videoInfo.FramesPerSecond);
+
+            hyperlapseParameters.SpeedupFactor = speedupFactor;
+
+            hyperlapseParameters.VideoUri = new Uri(fileInput.FullName);
+            hyperlapseParameters.VideoOutputFilePath = fileOutput.FullName;
+            hyperlapseParameters.TempOutputDirectory = fileOutput.DirectoryName;
+
+            hyperlapseParameters.CreditLength = 0;
+            hyperlapseParameters.UseAdvancedSmoothing = false;
+            hyperlapseParameters.ForceSoftwareRendering = false;
+            hyperlapseParameters.UseGeometryShaders = false;
+            hyperlapseParameters.UseHardwareVideoEncoder = false; // some videos fail when this is on
+
+            engine.Start(hyperlapseParameters);
+            processingEvent.WaitOne();
+        }
+
+        private static void OnEngineTrialStatusChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void OnEngineProgressChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void OnEngineProcessingFinished(object sender, ProcessingFinishedEventArgs e)
+        {
+            processingEvent.Set();
+        }
+
+        private static void OnEngineProcessingFailed(object sender, ProcessingFailedEventArgs e)
+        {
+            Console.WriteLine("Error: " + e.ErrorMessage);
+            processingEvent.Set();
+        }
+
+        private static void OnEngineProcessingCancelled(object sender, EventArgs e)
+        {
+            Console.WriteLine("[Cancelled]");
+            processingEvent.Set();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace HyperlapseBatchProcessor
+{
+
+    class Program {
+
+        private static readonly string[] ValidVideoExtensions = new string[] { "mp4", "wmv", "mov" };
+        private static readonly string HyperlapsePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Microsoft Hyperlapse Pro");
+
+        static Program()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolve;
+        }
+
+        static void Main(string[] args) {
+            try {
+                if (!Directory.Exists(HyperlapsePath))
+                {
+                    Console.WriteLine("Hyperlapse installation folder not found");
+                    return;
+                }
+
+                // parse arguments
+                int speedupFactor;
+                if (args.Length < 1) {
+                    Console.WriteLine("Wrong number of arguments. Usage:");
+                    Console.WriteLine("> " + Path.GetFileNameWithoutExtension(System.Diagnostics.Process.GetCurrentProcess().ProcessName) + " <speedup factor>");
+                    return;                
+                }
+
+                if (!int.TryParse(args[0], out speedupFactor)) {
+                    Console.WriteLine("Wrong speedup factor");
+                    return;
+                }
+
+                // get current directory
+                var baseDir = new DirectoryInfo(Environment.CurrentDirectory);
+                if (!baseDir.Exists) {
+                    Console.WriteLine("Directory '" + baseDir + "' does not exist");
+                    return;
+                }
+
+                // get video files to process
+                var videoFiles = ValidVideoExtensions.Aggregate(Enumerable.Empty<FileInfo>(), (files, extension) => files.Concat(baseDir.GetFiles("*." + extension))).ToArray();
+                if (videoFiles.Count() == 0) {
+                    Console.WriteLine("No video files found");
+                    return;
+                }
+
+                HyperlapseWrapper.ProcessFiles(videoFiles, speedupFactor, /*outputFramesPerSecond*/null);
+
+                Console.WriteLine("Finished");
+            } finally {
+                //Console.ReadLine();
+            }
+        }
+
+        private static Assembly AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var assemblyName = args.Name.Substring(0, args.Name.IndexOf(","));
+            if(assemblyName == "Microsoft.Research.Hyperlapse.Desktop")
+            {
+                assemblyName += ".exe";
+            } else
+            {
+                assemblyName += ".dll";
+            }
+            assemblyName = Path.Combine(HyperlapsePath, assemblyName);
+            return Assembly.LoadFrom(assemblyName);
+        }
+
+        
+    }
+}
